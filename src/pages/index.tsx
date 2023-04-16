b64508c787e1c57696328f0bb511200fcf75863f
projack14 <zakaria.zrs14@gmail.com>
2023-04-16 12:13:16 +0700

    update instalize

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import { FC } from "react";
 const Home: FC = () => {
   return (
     <div>
-      <p>HOMEPAGE</p>
+      <p className="font-bold text-7xl">HOMEPAGE</p>
+      <p className="font-bold text-7xl">1234567890</p>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      "md-to-lg": "960px",
+      // => @media (min-width: 640px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        "@19345E": "#19345E",
+        "@EBF2FA": "#EBF2FA",
+        "@679436": "#679436",
+        "@A5BE00": "#A5BE00",
+        "@427AA1": "#427AA1",
+        "@00B4D8": "#00B4D8",
+      },
+    },
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      DEFAULT: "4px",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+      yes: "2rem",
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
