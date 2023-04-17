03524c2da4703327825d9b19425b7cd1322a6009
projack14 <zakaria.zrs14@gmail.com>
2023-04-17 15:44:00 +0700

    feat home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,75 @@
 import { FC } from "react";
 import { Layout } from "../components/Layout";
+import { CardLanding } from "../components/Card";
 const Home: FC = () => {
   return (
     <Layout>
-      <div className="py-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-6 md:gap-8 xl:gap-10 p-4 sm:p-6 md:-8 xl:p-10">
-        <p className="font-bold text-7xl">HOMEPAGE</p>
+      <div className="px-6 py-6 md-to-lg:pb-10 md:pb-16 md:pt-8 md:px-10 lg:pb-20 lg:pt-10 lg:px-20 gap-1 ">
+        <h1 className="items-center font-bold text-2xl  md:texl-3xl lg:text-5xl  capitalize flex flex-col ">
+          Event Comingsoon
+        </h1>
+        <div className="py-10 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4 sm:gap-6 md:gap-8 xl:gap-10 p-4 sm:p-6 md:-8 xl:p-10">
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+          <CardLanding
+            event_image="./bg.jpg"
+            MyLink={`/detail-event`}
+            event_name="Webinar: Your Product Manager Career by PlayStation Sr PM"
+            event_date="Monday, 22 May 2023"
+          />
+        </div>
       </div>
     </Layout>
   );
